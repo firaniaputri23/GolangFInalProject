94b1eca3d4f64d9f23e40937e79f12626651ba45
adrianazizsantoso22 <adrian22pass@gmail.com>
2024-12-06 20:39:51 +0700

    Create message.go

---
+++ internal/models/message.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type Message struct {
+    gorm.Model
+    SenderID   uint   `json:"sender_id"`
+    ReceiverID uint   `json:"receiver_id"`
+    Content    string `json:"content"`
+    Attachment string `json:"attachment"`
+}
